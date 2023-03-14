033aa7d2722516a28476717e8ef87f3b086e764c
guanchang98 <guanchangst@163.com>
2023-03-14 13:54:15 -0700

    Auth controller set up successfully

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
 import express from "express";
 const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+
+// db and authenticate user
+import connectDB from "./db/connect.js";
+
+// routes
+import authRouter from "./routes/authRoutes.js";
+
+// middleware
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+import notFoundMiddleware from "./middleware/not-found.js";
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome!");
 });
 
+app.use('/api/v1/auth', authRouter);
+
+app.use(notFoundMiddleware);
+
+app.use(errorHandlerMiddleware);
+
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server is listening on ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
